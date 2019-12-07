935fbf18d9368b4b3bdb257d24e6954a02a8c9cc
takumi <tak.ranmaru@icloud.com>
2019-12-07 16:48:04 +0900

    Initial Commit

---
+++ MyTodoList/ViewController.swift
@@ -0,0 +1,118 @@
+//
+//  ViewController.swift
+//  MyTodoList
+//
+//  Created by takumi on 2019/12/07.
+//  Copyright © 2019 takumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var todoList = [String]()
+    
+      // デバック用
+//    var todoList = ["rrrr", "reefr", "hyh"]
+    
+    
+    // 画面リロード用のアウトレット設定
+    @IBOutlet weak var tvList: UITableView!
+    
+    @IBOutlet weak var tfAddTodo: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    // MARK: テーブルビュー設定
+    
+    // 必須１　セクションの数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+            return todoList.count
+        
+    }
+    
+    // 必須２　セルの設定
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cel =
+            tableView.dequeueReusableCell(
+                withIdentifier: "list",
+                for: indexPath)
+        
+        cel.textLabel?.text = todoList[indexPath.row]
+        
+        
+        return cel
+        
+    }
+    
+    // セル選択時
+    func tableView(_ tableView: UITableView,
+                   didSelectRowAt indexPath: IndexPath) {
+        
+        let cel = tableView.cellForRow(at: indexPath)
+        _ = cel?.textLabel?.text ?? ""
+        
+    }
+    
+    // セルの編集許可
+    func tableView(_ tableView: UITableView,
+                   canEditRowAt indexPath: IndexPath) -> Bool {
+        
+        return true
+    }
+    
+    // セルの削除（スワイプしたら）
+    func tableView(_ tableView: UITableView,
+                   commit editingStyle: UITableViewCell.EditingStyle,
+                   forRowAt indexPath: IndexPath) {
+        
+        
+        
+        if editingStyle == UITableViewCell.EditingStyle.delete {
+            
+            todoList.remove(at: indexPath.row)
+            tableView.deleteRows(
+                at: [indexPath as IndexPath],
+                with: UITableView.RowAnimation.middle)
+            
+        }
+    }
+
+    // セクションタイトル
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        return "やることリスト"
+    }
+    
+    // MARK: - アクションメソッド
+
+    // +ボタン押下処理
+    @IBAction func btAddTodo(_ sender: Any) {
+        
+//        let addList = tfAddTodo.text
+        
+        // TFが空文字なら追加しない
+        if tfAddTodo.text != "" {
+            todoList.append("\(tfAddTodo.text!)")
+            tfAddTodo.text = ""
+        } else {
+            return
+        }
+        
+        view.endEditing(true)
+        
+        // 追加し終わったらTableView再読み込み
+        tvList.reloadData()
+    }
+    
+    @IBAction func btAdd(_ sender: Any) {
+    }
+    
+}
+
